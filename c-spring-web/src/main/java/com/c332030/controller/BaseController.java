e24d45696cb2e4bc6bfb09c617e5f5074da69bfb
c332030 <c332030@gmail.com>
2020-09-04 12:39:02 +0800

    feat: print default charset

--- c-spring-web/src/main/java/com/c332030/controller/BaseController.java
+++ c-spring-web/src/main/java/com/c332030/controller/BaseController.java
@@ -2,15 +2,19 @@ package com.c332030.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.MediaType;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.google.common.base.Charsets;
-
 import lombok.extern.slf4j.Slf4j;
+
+import com.c332030.constant.sys.CharsetConstants;
+import com.c332030.util.data.StringUtils;
 
 /**
  * <p>
@@ -29,14 +33,20 @@ public abstract class BaseController {
     @Autowired
     protected HttpServletResponse response;
 
+    protected static final String DEFAULT_CONTEXT_TYPE = StringUtils.join(
+        new String[] {
+            MediaType.TEXT_HTML_VALUE, CharsetConstants.CHARSET_UTF8
+        }, ';'
+    );
+
     protected void print(String string) {
 
         try {
             PrintWriter printWriter = response.getWriter();
             printWriter.print(string);
 
-            response.setContentType("text/html;charset=utf-8");
-            response.setCharacterEncoding(Charsets.UTF_8.name());
+            response.setContentType(DEFAULT_CONTEXT_TYPE);
+            response.setCharacterEncoding(StandardCharsets.UTF_8.name());
         } catch (IOException e) {
             log.error("获取响应 Writer 失败", e);
         }
