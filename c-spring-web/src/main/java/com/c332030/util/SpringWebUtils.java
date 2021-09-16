216df81711a6bf2a289edf2fe139c362f69308c4
c332030 <c332030@gmail.com>
2021-09-16 21:04:52 +0800

    feat: RESPONSE_ENTITY_EMPTY

--- c-spring-web/src/main/java/com/c332030/util/SpringWebUtils.java
+++ c-spring-web/src/main/java/com/c332030/util/SpringWebUtils.java
@@ -1,6 +1,5 @@
 package com.c332030.util;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import lombok.AccessLevel;
@@ -17,20 +16,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SpringWebUtils {
 
-    public static final ResponseEntity<String> RESPONSE_ENTITY_EMPTY = new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
-
-    /**
-     * <p>
-     * Description: 获取成功的 newResponseEntity
-     * </p>
-     *
-     * @param t 内容
-     * @param <T> 内容类型
-     * @return ResponseEntity 实体
-     * @author c332030
-     */
-    public static <T> ResponseEntity<T> newResponseEntityOK(T t) {
-        return new ResponseEntity<>(t, HttpStatus.OK);
-    }
+    public static final ResponseEntity<?> RESPONSE_ENTITY_EMPTY = ResponseEntity.noContent().build();
 
 }
