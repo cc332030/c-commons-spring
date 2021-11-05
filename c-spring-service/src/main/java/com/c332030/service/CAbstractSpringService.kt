17c028d34ab7d33cf0c174d3def20b710d87378d
c332030 <c332030@gmail.com>
2021-11-05 14:57:07 +0800

    feat: kotlin

---
+++ c-spring-service/src/main/java/com/c332030/service/CAbstractSpringService.kt
@@ -0,0 +1,11 @@
+package com.c332030.service
+
+/**
+ * <p>
+ * Description: CAbstractSpringService
+ * </p>
+ *
+ * @author c332030
+ * @version 1.0
+ */
+abstract class CAbstractSpringService: CAbstractService()
